4391d21a6628ce9b8f7ef944c1653db285da179a
Pierre Lupien <pierre.lupien@hrsdc-rhdcc.gc.ca>
2017-04-11 11:32:02 -0400

    New 4.0.24 features implemented in both JSF projects

--- GoCWebTemplateSampleJSF2x/src/goc/webtemplate/jsf/samplebeans/ApplicationSample.java
+++ GoCWebTemplateSampleJSF2x/src/goc/webtemplate/jsf/samplebeans/ApplicationSample.java
@@ -1,20 +1,25 @@
-package goc.webtemplate.jsp.samplebeans;
+package goc.webtemplate.jsf.samplebeans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
 
 import goc.webtemplate.FooterLink;
 
-import goc.webtemplate.component.jsp.DefaultTemplateBean;
+import goc.webtemplate.component.jsf.DefaultTemplateBean;
 
-public class ApplicationSampleBean extends DefaultTemplateBean {
+@Named("applicationsamplebean")
+@RequestScoped
+public class ApplicationSample extends DefaultTemplateBean {
 
-	@Override
-	public void setHeaderTitle() {
-		this.headerTitle = "My Application Title";
-	}
+    @Override
+    public void setHeaderTitle() {
+        this.headerTitle = "My Application Title";
+    }
 
-	@Override
-	public void setApplicationTitleText() {
-	    this.applicationTitleText = "My Application";
-	}
+    @Override
+    public void setApplicationTitleText() {
+        this.applicationTitleText = "My Application";
+    }
     
     @Override
     public void setShowSecureIcon() {
@@ -44,17 +49,17 @@ public class ApplicationSampleBean extends DefaultTemplateBean {
 
     //NOTE: This can also be set at the application level by setting the property goc.webtemplate.customsitemenuurl in cdn.properties
     //      (in this sample, default in cdn.properties is blank, which means the default menu will be used)
-	@Override
-	public void setCustomSiteMenuUrl() {
-	    this.customSiteMenuUrl = this.getRequest().getContextPath() + "/samplecontents/mycustommenu.html";
-	}
+    @Override
+    public void setCustomSiteMenuUrl() {
+        this.customSiteMenuUrl = this.getRequest().getContextPath() + "/samples/mycustommenu.html";
+    }
     
     //NOTE: This can also be set at the application level by setting the property goc.webtemplate.signinlinkurl in cdn.properties
     //      (in this sample, default in cdn.properties is blank)
-	@Override
-	public void setSignInLinkUrl() {
-	    this.signInLinkUrl = "#"; //we don't really have a sign-in page/facility for the sample project, just link to self 
-	}
+    @Override
+    public void setSignInLinkUrl() {
+        this.signInLinkUrl = "#"; //we don't really have a sign-in page/facility for the sample project, just link to self 
+    }
     
     //NOTE: This can also be set at the application level by setting the property goc.webtemplate.signoutlinkurl in cdn.properties
     //      (in this sample, default in cdn.properties is blank)

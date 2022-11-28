198d8ef7dedbdc69d126f9882628fcc2928cc430
ahmad.shahid <ahmad.shahid@hrsdc-rhdcc.gc.ca>
2022-11-28 13:15:32 -0500

    Adding hidePlaceholderMenu parameter

--- gocwebtemplate-core/gocwebtemplate-core-base/src/main/java/goc/webtemplate/Constants.java
+++ gocwebtemplate-core/gocwebtemplate-core-base/src/main/java/goc/webtemplate/Constants.java
@@ -15,7 +15,7 @@ public abstract class Constants {
     
     public static final String WEB_TEMPLATE_DISTRIBUTION_VERSION = "2.4.0";
 
-    public static final String CDTS_DEFAULT_VERSION = "v4_0_45";
+    public static final String CDTS_DEFAULT_VERSION = "v4_0_46";
     
     public static final String STATIC_FALLBACK_FILES_INTERNAL_PATH = "/goc/webtemplate/StaticFallbackFiles";
     

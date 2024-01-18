ceefa5afb71614e6782bf12d5655a497721e5409
Pierre Lupien <pierre.lupien@hrsdc-rhdcc.gc.ca>
2024-01-18 13:51:51 -0500

    Bump of maven-war-plugin to latest version for compatibility with java17+

--- gocwebtemplate-core/gocwebtemplate-core-spring/src/test/java/goc/webtemplate/component/spring/DefaultTemplateCoreBeanTest.java
+++ gocwebtemplate-core/gocwebtemplate-core-spring/src/test/java/goc/webtemplate/component/spring/DefaultTemplateCoreBeanTest.java
@@ -3,7 +3,7 @@ package goc.webtemplate.component.spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = WebConfigSpring.class)
 class DefaultTemplateCoreBeanTest {
 	
 	//TODO: Complete this test case

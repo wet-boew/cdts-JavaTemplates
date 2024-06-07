58ff08a2214a209f5172de859f53fa1f5a1db2e9
ahmad.shahid <ahmad.shahid@hrsdc-rhdcc.gc.ca>
2024-06-07 16:26:13 -0400

    Updating version and changelog

--- gocwebtemplate-core/gocwebtemplate-core-base/src/main/java/goc/webtemplate/component/AbstractCoreBean.java
+++ gocwebtemplate-core/gocwebtemplate-core-base/src/main/java/goc/webtemplate/component/AbstractCoreBean.java
@@ -461,7 +461,7 @@ public abstract class AbstractCoreBean {
     }
     
     /**
-     * Returns the version of the CDN files to use to build the page. (e.g v5_0_0)
+     * Returns the version of the CDN files to use to build the page. (e.g v5_0_1)
      * 
      * Set at application level via "wettemplate_version" property in cdn.properties, 
      * can be overriden programatically.  
@@ -482,7 +482,7 @@ public abstract class AbstractCoreBean {
     }
 
     /**
-     * Sets the version of the CDN files to use to build the page. (e.g v5_0_0)
+     * Sets the version of the CDN files to use to build the page. (e.g v5_0_1)
      * 
      * Set at application level via "wettemplate_version" property in cdn.properties, 
      * can be overriden programatically.  

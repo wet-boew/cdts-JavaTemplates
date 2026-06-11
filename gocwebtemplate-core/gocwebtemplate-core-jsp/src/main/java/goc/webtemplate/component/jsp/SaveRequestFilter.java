4588f495fce86e69c329036c9042e7ddd250c958
ahmad.shahid <ahmad.shahid@hrsdc-rhdcc.gc.ca>
2026-06-11 10:26:24 -0400

    Update struts to 7.1.1

--- gocwebtemplate-core/gocwebtemplate-core-jsp/src/main/java/goc/webtemplate/component/jsp/SaveRequestFilter.java
+++ gocwebtemplate-core/gocwebtemplate-core-jsp/src/main/java/goc/webtemplate/component/jsp/SaveRequestFilter.java
@@ -1,12 +1,12 @@
 package goc.webtemplate.component.jsp;
 
 import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 
 public class SaveRequestFilter implements Filter {
 
